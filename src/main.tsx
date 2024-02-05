1ad35ea8a7669cb792b1dae3cbba8da88e8c3882
Jack Maders <jackwmaders@gmail.com>
2024-02-05 15:36:23 +0000

    project setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ChakraProvider, Container } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NotesApp from "./notesApp/NotesApp";
+import "./tailwind.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ChakraProvider>
+    <React.StrictMode>
+      <Container className="space-y-8 pt-8">
+        <h1 className="text-2xl font-semibold">Notes App</h1>
+        <NotesApp />
+      </Container>
+    </React.StrictMode>
+  </ChakraProvider>
+);
